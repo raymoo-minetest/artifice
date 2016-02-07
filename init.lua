2ef06b8cf81aba46b5aa1733a6cfe92a3d5da402
raymoo <uguu@installgentoo.com>
2016-02-07 00:55:18 -0800

    Put energy in init

--- init.lua
+++ init.lua
@@ -4,3 +4,5 @@ artifice.save = {}
 artifice.modpath = minetest.get_modpath(minetest.get_current_modname()) .. "/"
 
 dofile(artifice.modpath .. "util.lua")
+dofile(artifice.modpath .. "executor_reified.lua")
+dofile(artifice.modpath .. "energy/energy.lua")
