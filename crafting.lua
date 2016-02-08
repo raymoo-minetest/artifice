120b5fe2131ea7155384efef04bb879bdde185c6
raymoo <uguu@installgentoo.com>
2016-02-07 18:08:03 -0800

    mana dust

---
+++ crafting.lua
@@ -0,0 +1,6 @@
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "artifice:mana_dust",
+	recipe = { "artifice:mana_crystal 2" },
+})
---
+++ craftitems.lua
@@ -0,0 +1,8 @@
+
+-- Mana Dust
+
+minetest.register_craftitem("artifice:mana_dust", {
+	description = "Mana Dust",
+	inventory_image = "artifice_mana_dust.png",
+})
+
