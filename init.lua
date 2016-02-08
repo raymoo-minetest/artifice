11b1baa4f5c6b9e1a9c080daca422aeefec3bae1
raymoo <uguu@installgentoo.com>
2016-02-07 18:10:06 -0800

    Add mana dust

--- init.lua
+++ init.lua
@@ -6,4 +6,6 @@ artifice.modpath = minetest.get_modpath(minetest.get_current_modname()) .. "/"
 dofile(artifice.modpath .. "util.lua")
 dofile(artifice.modpath .. "executor_reified.lua")
 dofile(artifice.modpath .. "resources/resources.lua")
+dofile(artifice.modpath .. "craftitems.lua")
 dofile(artifice.modpath .. "energy/energy.lua")
+dofile(artifice.modpath .. "crafting.lua")
