6ed644ab0951fd733f106e7ab30bfd16d7df4b93
raymoo <uguu@installgentoo.com>
2016-02-08 20:18:58 -0800

    recipe for magnet

--- crafting.lua
+++ crafting.lua
@@ -1,6 +1,18 @@
 
+-- Resources
 minetest.register_craft({
 	type = "shapeless",
 	output = "artifice:mana_dust 2",
 	recipe = { "artifice:mana_crystal" },
+})
+
+
+-- Generators
+minetest.register_craft({
+	output = "artifice:mana_magnet",
+	recipe = {
+		{ "", "default:steel_ingot", ""},
+		{ "group:wood", "artifice:mana_crystal", "group:wood"},
+		{ "group:wood", "group:wood", "group:wood" },
+	},
 })
