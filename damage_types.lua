d3543f68dc668359253d4528f5cdf9669f318016
raymoo <uguu@installgentoo.com>
2016-02-10 20:39:09 -0800

    new damage types

---
+++ damage_types.lua
@@ -0,0 +1,7 @@
+
+armor_monoid.register_armor_group("arcane", 100)
+armor_monoid.register_armor_group("nature", 100)
+armor_monoid.register_armor_group("earth", 100)
+armor_monoid.register_armor_group("fire", 100)
+armor_monoid.register_armor_group("water", 100)
+armor_monoid.register_armor_group("air", 100)
--- init.lua
+++ init.lua
@@ -5,6 +5,7 @@ artifice.modpath = minetest.get_modpath(minetest.get_current_modname()) .. "/"
 
 dofile(artifice.modpath .. "util.lua")
 dofile(artifice.modpath .. "executor_reified.lua")
+dofile(artifice.modpath .. "damage_types.lua")
 dofile(artifice.modpath .. "resources/resources.lua")
 dofile(artifice.modpath .. "craftitems.lua")
 dofile(artifice.modpath .. "energy/energy.lua")
