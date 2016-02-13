f89b0e64a2ea7e377dacdc19ca2adc304e536ca4
raymoo <uguu@installgentoo.com>
2016-02-13 12:53:26 -0800

    Put xp and save into init '

--- init.lua
+++ init.lua
@@ -1,10 +1,10 @@
 artifice = {}
-artifice.save = {}
-
 artifice.modpath = minetest.get_modpath(minetest.get_current_modname()) .. "/"
 
+dofile(artifice.modpath .. "save.lua")
 dofile(artifice.modpath .. "util.lua")
 dofile(artifice.modpath .. "executor_reified.lua")
+dofile(artifice.modpath .. "player/player.lua")
 dofile(artifice.modpath .. "damage_types.lua")
 dofile(artifice.modpath .. "resources/resources.lua")
 dofile(artifice.modpath .. "craftitems.lua")
