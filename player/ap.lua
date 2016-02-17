adc0dd23a539a904df92bf57dd7eeb6b2acde489
raymoo <uguu@installgentoo.com>
2016-02-17 01:16:52 -0800

    Save fix in AP

--- player/ap.lua
+++ player/ap.lua
@@ -6,14 +6,13 @@ local function load_aps()
 	return (ser and minetest.deserialize(ser)) or {}
 end
 
+local aps = load_aps()
 
 local function save_aps(aps)
 	local ser = minetest.serialize(aps)
 	return artifice.save_data("ap", ser)
 end
 
-
-local aps = load_aps()
 
 -- An ability point record for a player is a table with these fields:
 --   points_spent: A nonnegative integer
@@ -28,19 +27,22 @@ function artifice.get_ap(p_name)
 	local rec = aps[p_name]
 	local base = calc_ap(artifice.get_level(p_name))
 
-	if not rec then return base end
-
+	if not rec then
+		return base
+	end
 	return base - rec.points_spent
 end
 
 
-local function set_spent(p_name, amt)
+local function change_spent(p_name, amt)
 	local rec = aps[p_name]
 	if not rec then
 		aps[p_name] = { points_spent = amt }
 	else
-		rec.points_spent = amt
+		rec.points_spent = amt + rec.points_spent
 	end
+
+	save_aps(aps)
 end
 
 
@@ -50,8 +52,6 @@ function artifice.subtract_ap(p_name, amt)
 
 	if limit < amt then return false end
 	
-	set_spent(p_name, limit - amt)
-
-	save_aps(aps)
+	change_spent(p_name, amt)
 	return true
 end
